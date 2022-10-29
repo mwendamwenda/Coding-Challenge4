19bd802e8cdc2fec38baedce8fff972c2dc1bd39
mwendamwenda <benmwenda83@gmail.com>
2022-10-29 09:41:58 +0300

    final code challenge submit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('coding is all we wanted to grow as africa');
+// console.log('coding is all we wanted to grow as africa');
+
+// const bill = 275;
+// const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.15 ;
+// console.log(`the bill was ${bill},the tip was ${tip},and the total was ${bill + tip}`); 
+
+// const bill = 40;
+// const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.20 ;
+// console.log(`the bill was ${bill},the tip was ${tip},and the total was ${bill + tip}`); 
+
+const bill = 430;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.20 ;
+console.log(`the bill was ${bill},the tip was ${tip},and the total was ${bill + tip}`); 
+
